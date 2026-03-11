2b6d43178bc4770dcc00f3bec75156407df6ea46
Pedro <pedro.202401435@alunocesul.com>
2026-03-10 22:31:12 -0300

    aula 2/2 10/03

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
-import { Button, Flex, Heading, HStack, Strong, VStack } from "@chakra-ui/react";
+import { Button, CardBody, CardFooter, CardRoot, Flex, Heading, HStack, Strong, VStack } from "@chakra-ui/react";
 import NextLink from "next/link"
 import { Link as ChakraLink } from "@chakra-ui/react"
 import { Image as ChakraImage } from "@chakra-ui/react"
 import NextImage from "next/image"
-import { LuCar, LuSparkles, LuStar } from "react-icons/lu";
+import { LuArrowRight, LuCar, LuShield, LuSparkle, LuSparkles, LuStar } from "react-icons/lu";
 import { Icon } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
 import car from "../../public/assets/car.jpeg"
 import { ColorModeButton } from "@/components/ui/color-mode";
 import { Tag } from "@/components/ui/tag";
-
-
-
+import { Card } from "@/components/home/card";
 
 export default function Home() {
 
@@ -19,13 +17,13 @@ export default function Home() {
       <>
       <HStack as ="header" position={"fixed"} left={0} right={0} top={0} zIndex={50} bg={"blackAlpha.800"}>
         <HStack as={"nav"} justify={"space-between"} px={8} py={4} w={"full"} alignItems={'center'}>
-        <ChakraLink alignItems={"center" } gap={2} asChild>
+        <ChakraLink colorPalette={"yellow"} alignItems={"center" } gap={2} asChild>
          <NextLink href="/">
           <Icon w={7} h={7}>
             <LuCar />
           </Icon>
 
-          <Text fontSize="xl" fontWeight ={"bold"}>autoShine</Text>
+          <Text fontSize="xl" fontWeight ={"bold"}>AutoShine</Text>
          </NextLink>
         </ChakraLink>
     
@@ -56,7 +54,7 @@ export default function Home() {
       </HStack>
 
        <VStack as ="main" gap={0}>
-        <VStack as ="section" position={"relative"} w={"100vw"} h={'100vh'}align={"start"} justify={"center"}>
+        <VStack as ="section" position={"relative"} w={"100vw"} h={'100vh'} align={"start"} justify={"center"}>
 
           <Flex position={"absolute"} zIndex={1} w={"100vw"} h={"100vh"} bgColor={"blackalpha.400/90"}/>
 
@@ -64,26 +62,86 @@ export default function Home() {
             <NextImage src={car} alt="AutoShine" />
           </ChakraImage>
 
-
           <VStack zIndex={2} maxW={"2xl"} align={'start'} position={"relative"} px={6} gap={0}>
-          <Tag startElement={<LuSparkles />} rounded={"lg"} px={4} py={2} fontSize={"sm"} fontWeight={"medium"}>Excelencia em Estética Automotiva</Tag>
+          <Tag colorPalette={"yellow"} startElement={<LuSparkles />} rounded={"lg"} px={4} py={2} fontSize={"sm"} 
+          fontWeight={"medium"}>Excelencia em Estética Automotiva</Tag>
 
-          <Heading as={"h1"} fontSize={{lg: "7xl", md:"5xl"}} fontWeight={"bold"} lineHeight={"normal"} letterSpacing={"tight"}>Seu carro merece o <strong>melhor</strong> cuidado </Heading>
+          <Heading as={"h1"} fontSize={{lg: "7xl", md:"5xl"}} fontWeight={"bold"} lineHeight={"shorter"}
+           letterSpacing={"tight"} mb={6}>Seu carro merece o <Text as="span" color="yellow.400">melhor</Text> cuidado </Heading>
 
           <Text maxW={"lg"} fontSize={"lg"} fontWeight={"medium"} mb={6}>Transformamos seu veiculo com serviços premium de estética automotiva 
             Agende online e garante o brilho que seu carro merece. 
           </Text>
 
-          <HStack>
-            <Button>Agendar Agora</Button>
-            <Button>Agendar Serviços</Button>
+          <HStack gap={4}>
+            <Button rounded={"lg"} bgColor={"yellow.400"}>
+              <LuCar />Agendar Agora</Button>
 
+            <Button rounded={"lg"}>Ver Serviços
+              <LuArrowRight />
+            </Button>
+          </HStack>
+        </VStack> 
+        </VStack>
+
+        <VStack>
+
+        <VStack as="section" py={24} gap={16}>
+          <VStack gap={4}>
+            <Heading as="h2" fontSize="4xl" fontWeight= "bold"> Nossos Serviços</Heading>
+            <Text maxW={"xl"} fontSize={"lg"} textAlign={"center"}>Oferemos os melhores serviços de estéticas automotivas para ader seu veiculo impecavel</Text>
+          </VStack> 
+
+          <HStack gap={6}>
+            <Card
+              icon={<LuSparkle color={"black"}/>}
+              title="Polimento Premium"
+              description="Restauração completa do brilho original da pintura com técnicas profissionais"
+              textFooter="A partir de R$1200.00"
+              
+            />
+             <Card
+              icon={<LuShield color={"black"}/>}
+              title="Vitrificação"
+              description="Proteção cerâmica de longa duração que mantém seu carro impecável por meses"
+              textFooter="A partir de R$800.00"
+              />
+
+               <Card
+              icon={<LuCar color={"black"}/>}
+              title="Lavagem Detalhada"
+              description="Limpeza completa interna e eterna com produtos de alta qualidade"
+              textFooter="A partir de R$120.00"
+              />
+               <Card
+              icon={<LuStar color={"black"}/>}
+              title="Higienização interna"
+              description="Limpeza profunda de vancos, carpetes e painéis com ozônio"
+              textFooter="A partir de R$250.00"
+              />
           </HStack>
 
-
-        </VStack>  
+          <Button colorPalette="yellow" rounded={"lg"}>
+          Agendar Serviço
+          <LuArrowRight/>
+          </Button>
         </VStack>
-      </VStack>   
+        </VStack>
+
+      </VStack>
+      <VStack as= "footer" borderTopWidth={1} py={12}>
+        <ChakraLink colorPalette={"yellow"} alignItems={"center" } gap={2} asChild>
+         <NextLink href="/">
+          <Icon w={7} h={7}>
+            <LuCar />
+          </Icon>
+
+          <Text fontSize="xl" fontWeight ={"bold"}>AutoShine</Text>
+         </NextLink>
+        </ChakraLink>
+
+        <Text fontSize='sm'>© 2026 AutoShine Estética Automotiva. Todos os direitos reservados.</Text>
+      </VStack>
       </>
   );
 }
