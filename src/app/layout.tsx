1463f1de986661a429c911c7d905dc1ff40f5f2d
Pedro <pedro.202401435@alunocesul.com>
2026-03-18 19:35:06 -0300

    testeDevelop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,23 @@
 import { Provider } from "@/components/ui/provider";
 
 import "../styles/globals.css";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 
-export default function RootLayout(props: { children: React.ReactNode }) {
-  const { children } = props
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <body>
-        <Provider>{children}</Provider>
+        <Provider>
+          <Header />
+          {children}
+          <Footer />
+        </Provider>
       </body>
     </html>
-  )
+  );
 }
--- src/components/home/card.tsx
+++ src/components/home/card.tsx
@@ -1,35 +1,36 @@
-import { CardBody, CardRoot, Flex, Heading, Text, CardFooter } from "@chakra-ui/react";
+import { CardBody, CardFooter, CardRoot, Flex, Heading, Text } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import { LuSparkles } from "react-icons/lu";
 
-interface CardProps{
-    icon: ReactNode;
-    title: string;
-    description: string;
-    textFooter: string;
+interface CardProps {
+  icon: ReactNode;
+  title: string;
+  description: string;
+  textFooter: string;
 }
 
-export function Card({ icon, title, description, textFooter}: CardProps){
-    return(
+export function Card({ icon, title, description, textFooter }: CardProps) {
+  return (
+    <CardRoot w={80} _hover={{ borderColor: "yellow.500/30" }} cursor="pointer">
+      <CardBody gap={2}>
+        <Flex
+          w={12}
+          h={12}
+          align="center"
+          justify="center"
+          bgColor="yellow.300"
+          rounded="lg"
+        >
+          {icon}
+        </Flex>
 
-         <CardRoot w={80} _hover={{ borderColor:"yellow.500/30"}} cursor={"pointer"}>
-                  <CardBody gap={2}>
-                    <Flex w={12} h={12} 
-                    bgColor={"yellow.300"} 
-                    justify={"center"} 
-                    align={"center"}
-                     rounded={"lg"}>
-                      
-                      {icon}
-                    </Flex>
-    
-                    <Heading as="h3" fontSize={"xl"} fontWeight={"semibold"}>{title}</Heading>
-    
-                    <Text fontSize={"sm"}>{description}</Text>
-                  </CardBody>
-                  <CardFooter>
-                    <Text fontSize={"sm"} fontWeight={"semibold"}> {textFooter}</Text>
-                  </CardFooter>
-                </CardRoot>
-    )
+        <Heading as="h3" fontSize="xl" fontWeight="semibold">{title}</Heading>
+
+        <Text fontSize="sm">{description}</Text>
+      </CardBody>
+      <CardFooter>
+        <Text fontSize="sm" fontWeight="semibold">{textFooter}</Text>
+      </CardFooter>
+    </CardRoot>
+  )
 }
